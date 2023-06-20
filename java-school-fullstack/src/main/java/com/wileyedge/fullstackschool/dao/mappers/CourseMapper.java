7b83fc42711e7352cbbb724455f8df68a7f3ec3c
Navyasriveerisetty <133081344+Navyasriveerisetty@users.noreply.github.com>
2023-06-20 21:12:54 +1000

    Update CourseMapper.java

--- java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/mappers/CourseMapper.java
+++ java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/mappers/CourseMapper.java
@@ -9,8 +9,7 @@ public class CourseMapper implements RowMapper<Course> {
     @Override
     public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
         //YOUR CODE STARTS HERE
-
-        Course result = new Course();
+    	Course result = new Course();
     	result.setCourseDesc(rs.getString("courseDesc"));
     	result.setCourseId(rs.getInt("cid"));
     	result.setCourseName(rs.getString("courseCode"));
