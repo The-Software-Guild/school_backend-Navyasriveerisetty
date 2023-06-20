a18d44fa3b7035410b4a96a9a0f27798afcd9d05
Navyasriveerisetty <133081344+Navyasriveerisetty@users.noreply.github.com>
2023-06-20 21:11:50 +1000

    Update TeacherMapper.java

--- java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/mappers/TeacherMapper.java
+++ java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/dao/mappers/TeacherMapper.java
@@ -10,14 +10,13 @@ public class TeacherMapper implements RowMapper<Teacher> {
     public Teacher mapRow(ResultSet rs, int rowNum) throws SQLException {
         //YOUR CODE STARTS HERE
 
-        Teacher t = new Teacher();
-    	
-    	t.setTeacherId(rs.getInt("tid"));
-		t.setTeacherFName(rs.getString("tFName"));
-		t.setTeacherLName(rs.getString("tLName"));
-		t.setDept(rs.getString("dept"));
-		
-		return t;
+        Teacher result = new Teacher();
+    	result.setDept(rs.getString("dept"));
+    	result.setTeacherFName(rs.getString("tFName"));
+    	result.setTeacherId(rs.getInt("tid"));
+    	result.setTeacherLName(rs.getString("tLName"));
+
+        return result;
 
         //YOUR CODE ENDS HERE
     }
