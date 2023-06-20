1be5f8a697139c0c0776408c7f42606ff5d87ce3
Navyasriveerisetty <133081344+Navyasriveerisetty@users.noreply.github.com>
2023-06-20 21:05:50 +1000

    Update StudentServiceImpl.java

--- java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/service/StudentServiceImpl.java
+++ java-school-fullstack/src/main/java/com/wileyedge/fullstackschool/service/StudentServiceImpl.java
@@ -1,8 +1,11 @@
 package com.wileyedge.fullstackschool.service;
 
+import com.wileyedge.fullstackschool.dao.CourseDao;
 import com.wileyedge.fullstackschool.dao.StudentDao;
+import com.wileyedge.fullstackschool.model.Course;
 import com.wileyedge.fullstackschool.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,22 +15,22 @@ public class StudentServiceImpl implements StudentServiceInterface {
     //YOUR CODE STARTS HERE
     @Autowired
     StudentDao studentDao;
-
+    @Autowired
+    CourseDao courseDao;
+    List<Student> students = null;
+    List<Course> courses = null;
+    
     public StudentServiceImpl(StudentDao studentDao) {
         this.studentDao = studentDao;
     }
-
-    @Autowired
-    CourseServiceImpl courseService;
-
-
 
     //YOUR CODE ENDS HERE
 
     public List<Student> getAllStudents() {
         //YOUR CODE STARTS HERE
 
-        return null;
+    	students = studentDao.getAllStudents();
+        return students;
 
         //YOUR CODE ENDS HERE
     }
@@ -35,7 +38,14 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public Student getStudentById(int id) {
         //YOUR CODE STARTS HERE
 
-        return null;
+    	Student student = null;
+    	try {
+    		student = studentDao.findStudentById(id);
+    	} catch (DataAccessException e) {
+    		student.setStudentFirstName("Student Not Found");
+    		student.setStudentLastName("Student Not Found");
+    	}
+    	return student;
 
         //YOUR CODE ENDS HERE
     }
@@ -43,7 +53,16 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public Student addNewStudent(Student student) {
         //YOUR CODE STARTS HERE
 
-        return null;
+    	Student s = null;
+    	
+    	if(student.getStudentFirstName().isBlank() || student.getStudentLastName().isBlank()) {
+    		student.setStudentFirstName("First Name blank, student NOT added");
+    		student.setStudentLastName("Last Name blank, student NOT added");
+    		return student;
+    	} else {
+    		s = studentDao.createNewStudent(student);
+    	}
+        return s;
 
         //YOUR CODE ENDS HERE
     }
@@ -51,7 +70,14 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public Student updateStudentData(int id, Student student) {
         //YOUR CODE STARTS HERE
 
-        return null;
+    	if(id != student.getStudentId()) {
+    		String response = "IDs do not match, student not updated";
+    		student.setStudentFirstName(response);
+    		student.setStudentLastName(response);
+    	} else {
+    		studentDao.updateStudent(student);
+    	}
+    	return student;
 
         //YOUR CODE ENDS HERE
     }
@@ -59,6 +85,7 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public void deleteStudentById(int id) {
         //YOUR CODE STARTS HERE
 
+    	studentDao.deleteStudent(id);
 
         //YOUR CODE ENDS HERE
     }
@@ -66,7 +93,28 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public void deleteStudentFromCourse(int studentId, int courseId) {
         //YOUR CODE STARTS HERE
 
-
+    	courses = courseDao.getAllCourses();
+    	boolean outcome = true;
+    	for(Student s : students) {
+    		if(s.getStudentId() == studentId) {
+    			if(s.getStudentFirstName().equals("Student Not Found")) {
+    				System.out.println("Student not found");
+    				outcome = false;
+    			}
+    		}
+    	}
+    	for(Course c : courses) {
+    		if(c.getCourseId() == courseId) {
+    			if(c.getCourseName().equals("Course Not Found")) {
+    				System.out.println("Course not found");
+    				outcome = false;
+    			}
+    		}
+    	}
+    	if(outcome) {
+    		studentDao.deleteStudentFromCourse(studentId, courseId);
+    		System.out.println("Student: " + studentId + " deleted from course: " + courseId);
+    	}
 
         //YOUR CODE ENDS HERE
     }
@@ -74,7 +122,31 @@ public class StudentServiceImpl implements StudentServiceInterface {
     public void addStudentToCourse(int studentId, int courseId) {
         //YOUR CODE STARTS HERE
 
-
+    	try {
+    		boolean outcome = true;
+        	for(Student s : students) {
+        		if(s.getStudentId() == studentId) {
+        			if(s.getStudentFirstName().equals("Student Not Found")) {
+        				System.out.println("Student not found");
+        				outcome = false;
+        			}
+        		}
+        	}
+        	for(Course c : courses) {
+        		if(c.getCourseId() == courseId) {
+        			if(c.getCourseName().equals("Course Not Found")) {
+        				System.out.println("Course not found");
+        				outcome = false;
+        			}
+        		}
+        	}
+        	if(outcome) {
+        		studentDao.deleteStudentFromCourse(studentId, courseId);
+        		System.out.println("Student: " + studentId + " added to course: " + courseId);
+        	}
+    	} catch(Exception e) {
+    		System.out.println("Student: " + studentId + " already enrolled in course: " + courseId);
+    	}
 
         //YOUR CODE ENDS HERE
     }
